4aca171bf3413adce3e2dd9d9ebd08c628cc77b7
Marlon Greig Cerin Durand <90740756+grey1524@users.noreply.github.com>
2024-07-10 19:06:10 -0500

    Conection MySQL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,12 @@
+require(`dotenv`).config();
 module.exports = {
-    app:{
-        port: process.env.PORT || 4000
+    app: {
+        port: process.env.PORT || 4000,
+    },
+    mysql:{
+        host: process.env.MYSQL_HOST || `localhost`,
+        user: process.env.MYSQL_USER || `root`,
+        password: process.env.MYSQL_PASSWORD || `greig.cd`,
+        database: process.env.MYSQL_DB || `bdparaapi`
     }
 }
